0722dfea6938593914b1ad46ca4994d30185eac3
tranhung8382 <tranhung8382@gmail.com>
2023-05-07 22:35:46 +0700

    codetiep07052023

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,68 +1,19 @@
 //class component
 //function component
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component {
 
 
-    state = {
-        name: 'Hung Tran',
-        address: '30 Van Bao',
-        age: 41
-    };
 
-
-
-
-
-    // handleClick(event) Viết theo arrow function ở dưới
-    handleClick = (event) => {
-        // console.log("Click me my button")
-        //console.log(event)
-
-        console.log("My name is", this.state.name);
-
-
-        //merge State => react class
-        this.setState({
-            name: 'Lucky Duke',
-            age: Math.floor((Math.random() * 100 + 1))
-        })
-    }
-
-    handleOnMouseOver(event) {
-        console.log(event.pageX)
-    }
-
-    handleOnChangeInput = (event) => {
-        // console.log(event, event.target.value)
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault()  //không refresh lại trang, không trả lại trang ban đầu
-        // alert('me')
-        console.log(this.state)
-    }
     //jsx
     render() {
         return (
             <div>my first component
 
-                My name iss {this.state.name} and I'm from {this.state.address} and I'm {this.state.age}
 
-                <button onMouseOver={this.handleOnMouseOver}>Hover Me</button>
-                <button onClick={this.handleClick}>Click Me</button>
-                {/* nếu ở trên vẫn viết theo hàm kiểu này handleClick(event) 
-                   chúng ta có thể viết arrow function lúc gọi như sau:
-                   <button onClick = {(event) => {this.handleClick(event)}}=>Click Me</button>
-                */}
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input type="text" onChange={(event) => this.handleOnChangeInput(event)} />
-                    <button>Submit</button>
-                </form>
+                <UserInfor></UserInfor>
             </div>
         );
     }
